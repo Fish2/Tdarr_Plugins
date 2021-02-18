2c7ee3ea4ea114eef967b4f14e9187889e0563de
fish2 <ghill13.fish2@gmail.com>
2021-02-18 19:06:07 +0000

    Update Tdarr_Plugin_FSH2_Fish4CleanSDH.js

--- Community/Tdarr_Plugin_FSH2_Fish4CleanSDH.js
+++ Community/Tdarr_Plugin_FSH2_Fish4CleanSDH.js
@@ -109,13 +109,10 @@ function plugin(file, librarySettings, inputs) {
           ffmpegCommandInsert += `-map -0:s:${subtitleIdx} `;
           response.infoLog += `☒Subtitle stream detected as being descriptive, removing. Stream 0:s:${subtitleIdx} \n`;
           convert = true;
-        } else if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'attachment') {
-          if (file.ffProbeData.streams[i].codec_name == 'ttf') {
-            extraArguments += `-map -0:t:${attachmentIdx} `;
-            response.infoLog += `☒Subtitle stream attachment detected as being descriptive, removing. Stream 0:t:${attachmentIdx} \n`;
+        } else if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'attachment') && (file.ffProbeData.streams[i].codec_name == 'ttf') {
+            extraArguments += `-map -0:t:${subtitleIdx} `;
+            response.infoLog += `☒Subtitle stream attachment detected as being descriptive, removing. Stream 0:t:${subtitleIdx} \n`;
             convert = true;
-          }
-          attachmentIdx++;
         }
       }
     } catch (err) {
